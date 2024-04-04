23eccce16685acc5026c5d8c8eef2b6c75d58864
francjpd <francjpd@gmail.com>
2024-04-04 11:04:08 +0200

    chore(packages): added redux, redux toolkit, react router dom, mui, vitest.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* Lets reset our browser's default css to align it with MUI */}
+    <CssBaseline /> 
     <App />
   </React.StrictMode>,
 )
