86acdb662b04c82928f495e11415ddf278f2c846
francjpd <francjpd@gmail.com>
2024-04-07 13:01:27 +0200

    feat(transitions): added transition between pages

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,27 +1,25 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Root from '../shared/Root/Root';
-import ErrorPage from '../shared/ErrorPage/ErrorPage';
-import { ListProductsPage } from '../features/ListProducts/ListProductsPage';
-import { ProductDetailsPage } from '../features/ProductDetails/ProductDetailsPage';
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../shared/Root/Root";
+import ErrorPage from "../shared/ErrorPage/ErrorPage";
+import { ListProductsPage } from "../features/ListProducts/ListProductsPage";
+import { ProductDetailsPage } from "../features/ProductDetails/ProductDetailsPage";
 
 const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: '/',
-                element: <ListProductsPage></ListProductsPage>
-            },
-            {
-                path: '/:id',
-                element: <ProductDetailsPage></ProductDetailsPage>
-
-            }
-        ],
-
-    }
+        element: <ListProductsPage></ListProductsPage>,
+      },
+      {
+        path: "/:id",
+        element: <ProductDetailsPage></ProductDetailsPage>,
+      },
+    ],
+  },
 ]);
 
-export default Router
+export default Router;
--- src/shared/Root/Root.tsx
+++ src/shared/Root/Root.tsx
@@ -1,16 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { useOutlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Breadcrumb from "../Header/Breadcrumbs";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Root() {
-    return (
-        <div className="container w-full mx-auto flex flex-col">
-            <section className="mb-8">
-                <Header>
-                    <Breadcrumb />
-                </Header>
-            </section>
-            <Outlet></Outlet >
-        </div>
-    )
+  const outlet = useOutlet();
+
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+    },
+    in: {
+      opacity: 1,
+    },
+    out: {
+      opacity: 0,
+    },
+  };
+
+  return (
+    <div className="container w-auto w-12/12 md:w-10/12 mx-auto flex flex-col">
+      <section className="mb-8">
+        <Header>
+          <Breadcrumb />
+        </Header>
+      </section>
+      <AnimatePresence mode={"wait"}>
+        <motion.div
+          key={location.pathname}
+          initial="initial"
+          animate="in"
+          exit="out"
+          transition={{ duration: 0.125 }} // Set the duration to 0.5 seconds
+          variants={pageVariants}
+        >
+          {outlet}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 }
