6fbb299e39104d3d6bc42c9bcc28ed21b956894b
francjpd <francjpd@gmail.com>
2024-04-07 12:59:20 +0200

    feat(button): added button variants

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,7 +1,39 @@
 import { MouseEventHandler } from "react";
 
-export default function Button({ children, onClick }: { children?: React.ReactNode, onClick: MouseEventHandler<HTMLButtonElement> }) {
-    return (
-        <button type="button" onClick={onClick} className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{children}</button>
-    )
+type ButtonInput = {
+  children?: React.ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  prependIcon?: React.ReactNode;
+  className?: string;
+  variant?: "primary" | "secondary";
+};
+
+export default function Button({
+  children,
+  onClick,
+  prependIcon,
+  variant = "primary",
+}: ButtonInput) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`inline-flex px-3 py-2 rounded-sm ${
+        variant === "primary"
+          ? "bg-floralWhite-200  border-casper-600 text-casper-800  hover:bg-floralWhite-300 hover:border-floralWhite-600 placeholder-floralWhite-600 dark:bg-charlestonGreen-700 dark:border-charlestonGreen-900 dark:text-gray-100 dark:hover:bg-charlestonGreen-300 dark:hover:border-charlestonGreen-300"
+          : "bg-casper-200  border-casper-600 text-floralWhite-600  hover:bg-casper-300 hover:border-casper-600 placeholder-floralWhite-600  dark:bg-casper-950  dark:border-charlestonGreen-900 dark:hover:bg-charlestonGreen-300 dark:hover:border-charlestonGreen-300"
+      }`}
+    >
+      {prependIcon && (
+        <div
+          className={
+            variant === "primary" ? "text-casper-500" : "text-floralWhite-600"
+          }
+        >
+          {prependIcon}
+        </div>
+      )}
+      {children}
+    </button>
+  );
 }
