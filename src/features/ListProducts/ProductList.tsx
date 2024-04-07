3a10b592341e622316295782193e4c57488312e8
francjpd <francjpd@gmail.com>
2024-04-07 21:34:37 +0200

    style(productListPage): tweaked themes and styles

--- src/features/ListProducts/ProductList.tsx
+++ src/features/ListProducts/ProductList.tsx
@@ -7,11 +7,11 @@ export default function ProductList({ elements, onProductSelect }: { elements: P
 
             <div key={product.id} onClick={() => onProductSelect(product)}>
                 <div className=" cursor-pointer  hover:scale-110
-            hover:z-10 transform-gpu transition ease-in-out">
-                    <div className=" bg-gray-200  dark:bg-gray-900 dark:border-gray-600 dark:text-white p-2 pt-4 mt-0 rounded-sm  flex flex-col w-full h-48 items-center" >
-                        <img src={product.imgUrl} className="object-cover h-4/6 w-11/12" />
-                        <div className="text-center"><span className="text-sm" >{product.name} </span> / <span className="text-xs"> {product.binomialName}</span></div>
-                        <div><span>{product.price}</span> <span className="text-xs">eur</span></div>
+            hover:z-10 transform-gpu transition ease-in-out shadow-sm hover:shadow-md">
+                    <div className="bg-casper-50 dark:bg-charlestonGreen-700 text-casper-950   dark:border-gray-600 dark:text-white p-2 pt-4 mt-0 rounded-sm  flex flex-col sm:w-9/12 md:w-full h-56 items-center" >
+                        <img src={product.imgUrl} className="object-cover h-4/6 w-6/12 md:w-10/12 rounded-md" />
+                        <div className="text-center"><span className="text-md" >{product.name} </span> <span className="text-floralWhite-700"><hr className="border-floralWhite-300" /></span> <span className="text-xs"> {product.binomialName}</span></div>
+                        <div><span>{product.price}</span> <span className="text-xs text-floralWhite-700">eur</span></div>
                     </div>
                 </div>
             </div>
--- src/features/ListProducts/Search.tsx
+++ src/features/ListProducts/Search.tsx
@@ -1,21 +1,25 @@
+import Textfield from "../../shared/Textfield/Textfield";
+import SearchIcon from "@mui/icons-material/Search";
+
 type SearchProps = {
-    placeholder: string,
-    handleSearch: (value: string) => void
-}
+  placeholder: string;
+  handleSearch: (value: string) => void;
+};
 
 export default function Search({ handleSearch, placeholder }: SearchProps) {
-
-    return (
-        <form className="flex items-center max-w-sm mx-auto" onSubmit={(e) => e.preventDefault()}>
-            <div className="relative w-full">
-                <input type="text" name='search' onChange={(e) => handleSearch(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-100 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} />
-            </div>
-
-        </form>
-
-
-
-
-
-    )
+  return (
+    <form
+      className="flex items-end w-auto flex-row justify-end text-right content-end"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <div className="relative w-full  md:max-w-96">
+        <Textfield
+          prependIcon={<SearchIcon />}
+          placeholder={placeholder}
+          variant="secondary"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+    </form>
+  );
 }
