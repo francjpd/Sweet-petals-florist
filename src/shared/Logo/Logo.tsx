3486f5e252c7bd79e3fa448f4a61b2aaa31ad8c8
francjpd <francjpd@gmail.com>
2024-04-07 12:59:02 +0200

    feat(logo): added logo component

---
+++ src/shared/Logo/Logo.tsx
@@ -0,0 +1,24 @@
+import flower from "../../assets/flower.svg";
+
+type LogoInput = {
+  className?: string;
+};
+
+export default function Logo({ className }: LogoInput) {
+  return (
+    <div
+      className={`flex flex-col justify-center w-28 ${className && className}`}
+    >
+      <span className="inline-flex align-middle">
+        <img className="w-4 h-4 mr-1 mt-1" src={flower} alt="flower logo" />
+        <span className="text-floralWhite-600 font-extrabold text-md">
+          Floristeria
+        </span>
+      </span>
+
+      <span className="text-xs dark:text-white ps-5">
+        <span className="font-thin"> Dulces petalos</span>
+      </span>
+    </div>
+  );
+}
