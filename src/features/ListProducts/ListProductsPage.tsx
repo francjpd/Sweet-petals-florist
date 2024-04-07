ad5d69c9914d305aa971934f5021f422317c449e
francjpd <francjpd@gmail.com>
2024-04-07 19:27:55 +0200

    feat(ListProductsPage): added debounce & added portal to move search to the Header section

--- src/features/ListProducts/ListProductsPage.tsx
+++ src/features/ListProducts/ListProductsPage.tsx
@@ -1,42 +1,59 @@
 import { useNavigate } from "react-router-dom";
-import { Product, useFetchProductsQuery } from "../Products/ProductsApiSlice"
+import { Product, useFetchProductsQuery } from "../Products/ProductsApiSlice";
 import Search from "./Search";
 import { useState } from "react";
 import ProductList from "./ProductList";
+import debounce from "debounce";
+import { createPortal } from "react-dom";
 
 export function ListProductsPage() {
-    const { data } = useFetchProductsQuery()
-    const navigate = useNavigate();
-    const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-    const onProductSelect = async ({ id }: Product) => {
-        navigate(`/${id}`)
+  const { data } = useFetchProductsQuery();
+  const navigate = useNavigate();
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const onProductSelect = async ({ id }: Product) => {
+    navigate(`/${id}`);
+  };
+
+  // I would normally use a  debounce function to handle real time filtering.
+  const handleSearch = debounce((value: string) => {
+    if (value.length <= 1) {
+      setFilteredProducts([]);
+      return;
     }
-
-    // I would normally use a  debounce function to handle real time filtering.
-    const handleSearch = (value: string) => {
-        if (value.length <= 1) {
-            setFilteredProducts([])
-            return
-        }
-        if (data) {
-            setFilteredProducts(data.filter(product => {
-                const productName = product.name.toLowerCase()
-                return productName.includes(value.toLowerCase()) || product.binomialName.includes(value.toLowerCase())
-            }))
-        }
+    if (data) {
+      setFilteredProducts(
+        data.filter((product) => {
+          const productName = product.name.toLowerCase();
+          const binomialName = product.binomialName.toLowerCase();
+          const valueToCompare = value.toLowerCase();
+          return (
+            productName.includes(valueToCompare) ||
+            binomialName.includes(valueToCompare)
+          );
+        })
+      );
     }
+  }, 100);
 
-    return (<>
+  const element = document.getElementById("header-portal-slot");
 
-        <div className="w-12/12 my-8 sticky top-4 z-30  rounded-sm  flex flex-col  items:center md:items-end">
-            <div className="bg-gray-200 dark:bg-gray-900 p-2 w-12/12 lg:w-4/12 ">
-                <Search placeholder="Search" handleSearch={handleSearch}></Search>
-            </div>
-        </div>
-
-        <div className="">
-            {data && <ProductList elements={filteredProducts.length ? filteredProducts : data} onProductSelect={onProductSelect}></ProductList>}
-        </div>
-
-    </>)
+  return (
+    <>
+      <>
+        {element &&
+          createPortal(
+            <Search placeholder="Search" handleSearch={handleSearch}></Search>,
+            element
+          )}
+      </>
+      <div>
+        {data && (
+          <ProductList
+            elements={filteredProducts.length ? filteredProducts : data}
+            onProductSelect={onProductSelect}
+          ></ProductList>
+        )}
+      </div>
+    </>
+  );
 }
